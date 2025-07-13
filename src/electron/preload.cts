ab8c2afffe98ac379778c170fe08fa71cafce255
Adool <adelsaid284@gmail.com>
2025-07-13 17:39:54 +0300

    first upload in electron js

---
+++ src/electron/preload.cts
@@ -0,0 +1,7 @@
+
+const electron=require("electron");
+electron.contextBridge.exposeInMainWorld("electron",{
+    subscribeStatistics:(callback:(statistic:any)=>void)=>callback({}),
+    getStaticData:()=>console.log('static'),
+    
+})
---
+++ src/electron/resourceManger.ts
@@ -0,0 +1,46 @@
+import osUtils from "os-utils";
+import fs from 'fs';
+import os from 'os';
+import { BrowserWindow } from "electron";
+const POLLING_INTERVAL = 500;
+
+export function pollResources(mainWindow:BrowserWindow) {
+  setInterval(async() => {
+  const  cpuUsage=await getCpuUsage();
+  const ramUsage=getRamUsage();
+  const storageData=getStorageData();
+  mainWindow.webContents.send("statistics",{cpuUsage,ramUsage, storageUsage:storageData.usage})
+  }, POLLING_INTERVAL);
+} 
+
+function getCpuUsage(){
+return new Promise(resolve=>{
+    osUtils.cpuUsage(resolve)
+})
+//  osUtils.cpuUsage((percentage) => {
+//       console.log(`CPU Usage: ${(percentage * 100).toFixed(2)}%`);
+//     });
+}
+export function getStaticData(){
+    const totlaStorage=getStorageData().total;
+    const CpuModel=os.cpus()[0].model;
+    const totalMemoryGB=Math.floor(osUtils.totalmem()/1024);
+    return{
+        totlaStorage, totalMemoryGB,CpuModel
+    }
+}
+
+function getRamUsage(){
+    return 1- osUtils.freememPercentage();
+}
+
+function getStorageData(){
+    const stats=fs.statfsSync(process.platform==='win32'?'c://':'/');
+    const total=stats.bsize*stats.blocks;
+    const free =stats.bsize*stats.bfree;
+
+    return{
+        total:Math.floor(total/1_000_000_000),
+        usage:1-free/total
+    };
+}
